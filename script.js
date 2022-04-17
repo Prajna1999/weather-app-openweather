0b84bb509c5de0cb38a45a729d9d5017ac7d1e0b
Prajna1999 <gituprajna20@gmail.com>
2022-04-17 14:54:58 +0530

    add: location access

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ const wrapper=document.querySelector(".wrapper");
 const inputPart=document.querySelector(".input-part");
 const infoText=document.querySelector(".info-text");
 const inputField=document.querySelector(".input-box input");
+const locationBtn=document.querySelector("#location-btn");
+
+// add event listener to te location btn.
+locationBtn.addEventListener("click", ()=>{
+    if(!navigator.geolocation){
+        alert("Update your browser you dumb fool!")
+    }else{
+        navigator.geolocation.getCurrentPosition(onSuccess, onFailure);
+    }
+})
+
+function onSuccess(position){
+    console.log(position);
+}
+function onFailure(error){
+    infoText.innerText="Location Access Denied!";
+    infoText.classList.add("error");
+}
+
+
+
 
 // add keyup event listener to the inputField element.
 inputField.addEventListener("keyup", (e)=>{
