15c8e710984e14a1cfacb5c90fb621fdc0df90c3
Prajna1999 <gituprajna20@gmail.com>
2022-04-17 11:47:48 +0530

    initial api wiring

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
-// const root="https://api.openweathermap.org/data/2.5/weather?";
-
-
-// // document.querySelector("button").addEventListener("click", clickEventHandler)
-
-
-// function clickEventHandler(){
-
-//     const options={
-//         method:"GET",
-//         mode:"cors",
-//         params:{
-//             "q":"London",
-//             "appid":"1c0e8e1fdc74b7cd8e3d09e96e33f94f"
-//         }
-        
-//     }
-//     const query=Object.keys(options.params)
-//                 .map(k=>encodeURIComponent(k)+"="+encodeURIComponent(options.params[k]))
-//                 .join("&");
-
-//     const uri=root+query;
-
-//     const request=new Request(uri, options);
-//     fetch(request)
-//         .then((result)=>{
-//             if(!result.ok){
-//                 throw new Error("Bad Request");
-//             }
-//             return result.json();
-//         }).then((data)=>console.log(data.main))
-//         .catch((e)=>console.log("Error: ", e.message));
-
-// }
-
-
-
-
-
 const wrapper=document.querySelector(".wrapper");
 const inputPart=document.querySelector(".input-part");
 const infoText=document.querySelector(".info-text");
 const inputField=document.querySelector(".input-box input");
+
+// add keyup event listener to the inputField element.
+inputField.addEventListener("keyup", (e)=>{
+    if(e.key==='Enter' && inputField.value!==""){
+        // call the api.
+        requestApi(inputField.value);
+    }
+});
+
+// declare requestAPI function.
+function requestApi(city){
+    const root="https://api.openweathermap.org/data/2.5/weather?";
+    const options={
+        method:"GET",
+        mode:"cors",
+        params:{
+            "q":inputField.value,
+            "appid":"1c0e8e1fdc74b7cd8e3d09e96e33f94f"
+        }
+
+    };
+    const query=Object.keys(options.params)
+                    .map(k=>encodeURIComponent(k)+"="+encodeURIComponent(options.params[k]))
+                    .join("&");
+    const uri=root+query;
+
+    const request=new Request(uri, options)
+    // fetch the weather app data.
+    fetch(request).then((result)=>{
+        if(!result.ok){
+            throw new Error("ERROR: It's not you, it's us!");
+        }else{
+            return  result.json();
+        }
+       
+
+    }).then((result)=>{
+
+    }).catch((e)=>console.log(e.message))
+}
